7caebd3b532d67339b9604d20de85fcba384bcff
Luis Cadillo <luiscaf3r@gmail.com>
2022-12-02 22:06:05 -0500

    solve: 2b

--- 2022/src/day02/index.ts
+++ 2022/src/day02/index.ts
@@ -2,54 +2,61 @@ import run from "aocrunner"
 
 const parseInput = (rawInput: string) => rawInput
 
-const elfPoints = {
-  A: 1,
-  B: 2,
-  C: 3,
+const elfMoves = {
+  A: "rock",
+  B: "paper",
+  C: "scissors",
 }
-const mePoints = {
-  X: 1, // rock
-  Y: 2, // paper
-  Z: 3, // scissors
+const movePoints = {
+  rock: 1,
+  paper: 2,
+  scissors: 3,
 }
 const gameResultsPoints = {
   lost: 0,
   draw: 3,
   win: 6,
 }
-
 const part1 = (rawInput: string) => {
+  const meMoves = {
+    X: "rock",
+    B: "paper",
+    C: "scissors",
+  }
+
   const input = parseInput(rawInput)
   const games = input.split(/\n/)
   console.log(games)
   let totalPoints: number = 0
   for (let game of games) {
-    const elfMove = elfPoints[game[0] as keyof typeof elfPoints]
-    const meMove = mePoints[game[2] as keyof typeof mePoints]
+    const elfMove = elfMoves[game[0] as keyof typeof elfMoves]
+    const meMove = meMoves[game[2] as keyof typeof meMoves]
 
     const lost =
-      (elfMove === 1 && meMove === 3) ||
-      (elfMove === 2 && meMove === 1) ||
-      (elfMove === 3 && meMove === 2)
+      (elfMove === "rock" && meMove === "scissors") ||
+      (elfMove === "paper" && meMove === "rock") ||
+      (elfMove === "scissors" && meMove === "paper")
     const draw = elfMove === meMove
     const win =
-      (meMove === 1 && elfMove === 3) ||
-      (meMove === 2 && elfMove === 1) ||
-      (meMove === 3 && elfMove === 2)
+      (meMove === "rock" && elfMove === "scissors") ||
+      (meMove === "paper" && elfMove === "rock") ||
+      (meMove === "scissors" && elfMove === "paper")
 
     if (lost) {
-      totalPoints += gameResultsPoints["lost"] + meMove
+      totalPoints += gameResultsPoints["lost"] + movePoints[meMove]
 
       continue
     }
     if (draw) {
-      totalPoints += gameResultsPoints["draw"] + meMove
+      totalPoints +=
+        gameResultsPoints["draw"] +
+        movePoints[meMove as keyof typeof movePoints]
 
       continue
     }
 
     if (win) {
-      totalPoints += gameResultsPoints["win"] + meMove
+      totalPoints += gameResultsPoints["win"] + movePoints[meMove]
 
       continue
     }
@@ -60,8 +67,58 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
+  const games = input.split(/\n/)
+  const gameResultsDict = {
+    X: "lost",
+    Y: "draw",
+    Z: "win",
+  }
 
-  return
+  let totalPoints: number = 0
+
+  for (let round of games) {
+    const elfMove = elfMoves[round[0] as keyof typeof elfMoves]
+    const roundResult =
+      gameResultsDict[round[2] as keyof typeof gameResultsDict]
+    const resultPoints =
+      gameResultsPoints[roundResult as keyof typeof gameResultsPoints]
+
+    if (elfMove === "rock") {
+      const responseMove =
+        roundResult === "lost"
+          ? "scissors"
+          : roundResult === "draw"
+          ? "rock"
+          : "paper"
+      totalPoints += resultPoints + movePoints[responseMove]
+
+      continue
+    }
+    if (elfMove === "paper") {
+      const responseMove =
+        roundResult === "lost"
+          ? "rock"
+          : roundResult === "draw"
+          ? "paper"
+          : "scissors"
+      totalPoints += resultPoints + movePoints[responseMove]
+
+      continue
+    }
+    if (elfMove === "scissors") {
+      const responseMove =
+        roundResult === "lost"
+          ? "paper"
+          : roundResult === "draw"
+          ? "scissors"
+          : "rock"
+      totalPoints += resultPoints + movePoints[responseMove]
+
+      continue
+    }
+  }
+
+  return totalPoints
 }
 
 run({
